a0edc5c0331531e53d33adfbae125efc5041e151
Ira-Nikolaichuk <iranikolaichuk9@gmail.com>
2023-09-09 23:28:32 +0300

    4

--- a1.py
+++
@@ -1,9 +0,0 @@
-# x = 3
-# y = 3
-# print(id(x))
-# print(id(y))
-
-import math
-# print(math.pow(3., 3))
-
-a = float(input)
--- main.py
+++ main.py
@@ -3,11 +3,18 @@
 # print("Hello Git!")
 
 # важливо думати над логікою виходу із циклу
-counter = 0     # цикл while + лічильник
-while counter < 5: # або while True:
-    user_input = input(">>> ")
-    counter += 1 # counter = counter + 1
-    if user_input == "exit" or counter >= 5:
-        break
-    else:
-        print(f'You write {user_input}')
+# counter = 0     # цикл while + лічильник
+# while counter < 5: # або while True:
+#     user_input = input(">>> ")
+#     counter += 1 # counter = counter + 1
+#     if user_input == "exit" or counter >= 5:
+#         break
+#     else:
+#         print(f'You write {user_input}')
+
+age = input("How old are you? ")
+age = int(age)
+if age >= 18:
+    print("access allowed")
+else:
+    print("Access denied")
