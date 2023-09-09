f6b33b144e594c5240f6f6843a62c4416dcdf482
Ira-Nikolaichuk <142692498+Ira-Nikolaichuk@users.noreply.github.com>
2023-09-09 23:23:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print("Hello world!")
+# print("Hello world!")
 
-print("Hello Git!")
+# print("Hello Git!")
+
+# важливо думати над логікою виходу із циклу
+counter = 0     # цикл while + лічильник
+while counter < 5: # або while True:
+    user_input = input(">>> ")
+    counter += 1 # counter = counter + 1
+    if user_input == "exit" or counter >= 5:
+        break
+    else:
+        print(f'You write {user_input}')
+
+age = input("How old are you? ")
+if age >= 18:
+    print("access allowed")
+else:
+    print("Access denied")
