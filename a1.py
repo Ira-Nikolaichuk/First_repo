5886d52fb23f803412257c3d89cd83ab11fab403
Ira-Nikolaichuk <iranikolaichuk9@gmail.com>
2023-09-09 23:28:04 +0300

    4

---
+++ a1.py
@@ -0,0 +1,9 @@
+# x = 3
+# y = 3
+# print(id(x))
+# print(id(y))
+
+import math
+# print(math.pow(3., 3))
+
+a = float(input)
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("Hello world!")
+# print("Hello world!")
 
-print("Hello Git!")
+# print("Hello Git!")
+
+# важливо думати над логікою виходу із циклу
+counter = 0     # цикл while + лічильник
+while counter < 5: # або while True:
+    user_input = input(">>> ")
+    counter += 1 # counter = counter + 1
+    if user_input == "exit" or counter >= 5:
+        break
+    else:
+        print(f'You write {user_input}')
